52944a1306d94e6b204c3d3a573317e5dc47168c
moskovskaya <mary.moskovskaja@yandex.ru>
2020-10-12 17:19:38 +0300

    Исправления ColumnChart компонент

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -16,60 +16,53 @@ export default class ColumnChart {
   }
 
   destroy() {
-    for (const prop in this) {
-      if (this.hasOwnProperty(prop)) {
-        delete this[prop];
-      }
-    }
+    this.remove();
+    this.element = null;
+    this.subElements = {};
   }
 
   remove() {
     this.element.remove();
   }
 
+  getColumnChartClass() {
+    if (this.data.length) {
+      return "column-chart";
+    }
+    return "column-chart column-chart_loading";
+  }
+
+  createData() {
+    return this.data.reduce((result, item) => result += this.createColumn(item), '');
+  }
+
+  createColumn(value) {
+    const height = Math.floor(value * this.chartHeight / this.maxValue);
+    const percent = (value / this.maxValue * 100).toFixed(0) + "%";
+    return `<div style="--value: ${height}" data-tooltip=${percent}></div>`;
+  }
+
   render() {
     const columnChart = document.createElement('div');
-    columnChart.className = getColumnChartClass.call(this);
+    columnChart.className = this.getColumnChartClass();
     columnChart.style = `--chart-height: ${this.chartHeight}`;
-    const maxValue = Math.max.apply(null, this.data);
+
+    this.maxValue = Math.max.apply(null, this.data);
 
     columnChart.innerHTML = `<div class="column-chart__title">
                                ${'Total ' + this.label}
-                               ${createLink.call(this)}
+                               ${this.link ? `<a class="column-chart__link" href=${this.link}>View all</a>` : ''}
                              </div>
                              <div class="column-chart__container">
                                <div data-element="header" class="column-chart__header">
                                  ${this.value}
                                </div>
                                <div data-element="body" class="column-chart__chart">
-                                 ${createData.call(this)}
+                                 ${this.createData()}
                                </div>
                              </div>`;
 
     this.element = columnChart;
-
-    function createLink() {
-      if (this.link) {
-        return `<a class="column-chart__link" href=${this.link}>View all</a>`;
-      }
-      return '';
-    }
-
-    function getColumnChartClass() {
-      if (this.data.length) {
-        return "column-chart";
-      }
-      return "column-chart column-chart_loading";
-    }
-
-    function createData() {
-      return this.data.reduce((result, item) => result += createColumn.call(this, item), '');
-    }
-
-    function createColumn(value) {
-      const height = Math.floor(value * this.chartHeight / maxValue);
-      const percent = (value / maxValue * 100).toFixed(0) + "%";
-      return `<div style="--value: ${height}" data-tooltip=${percent}></div>`;
-    }
+    this.subElements = this.element.childNodes;
   }
 }
